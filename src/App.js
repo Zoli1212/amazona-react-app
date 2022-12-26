a28187bfed610402a8356d6306d33feb91029fb5
Your Name <you@example.com>
2022-12-26 20:17:11 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { HomeScreeen } from './components/HomeScreen';
+import { ProductScreen } from './components/ProductScreen';
 function App() {
   return (
+    <Router>
+
     <div>
       <header>
-        <a href="/">amazona</a>
+        <Link to="/">amazona</Link>
       
       </header>
       <main>
-        list products
+        <Routes>
+          <Route path='/product/:slug' element={<ProductScreen />} />
+          <Route path='/' element={<HomeScreeen/>} />
+
+        </Routes>
+        
+    
       </main>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import data from '../data'
+
+export const HomeScreeen = () => {
+  return (
+    <div>    
+      <h1>Featured products</h1>
+    <div className="products">
+
+    {
+      data.products.map(product => (<div key={product.slug} className="product">
+        <Link to={`/product/${product.slug}`}>
+
+        <img src={product.image} alt={product.name} />
+        </Link>
+        <div className="product-info">
+        <Link to={`/product/${product.slug}`}>
+
+        <p>
+          {product.name}
+
+        </p>
+        </Link>
+        <p>
+          <strong>
+
+          ${product.price}
+          </strong>
+
+        </p>
+        <button>Add to cart</button>
+
+        </div>
+
+      </div>))
+    }
+    </div></div>
+  )
+}
